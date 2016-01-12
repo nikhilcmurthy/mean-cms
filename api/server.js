b7906b34e525eb3094515e01a3d60f427c1707e6
Jason Watmore <cornflourblue@hotmail.com>
2016-01-13 10:50:30 +1100

    Create default admin user on startup if users collection is empty

--- api/server.js
+++ api/server.js
@@ -41,6 +41,14 @@ app.use('/authenticate', require('./routes/authenticate'));
 app.use('/users', require('./routes/users'));
 app.use('/uploads', require('./routes/uploads'));
 
+// create default admin user if 'users' collection is empty
+db.get('users').count({}, function (err, count) {
+    if (count === 0) {
+        console.log('users collection empty so creating default admin user (admin:admin)');
+        db.get('users').insert({ firstName: 'Default', lastName: 'Admin', username: 'admin', hash: bcrypt.hashSync('admin', 10) });
+    }
+});
+
 http.listen(3001, function () {
     console.log('API server listening on port 3001');
 });
